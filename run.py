efa95f83c538f2c75b1e672eeb0a257aaa870c90
Егор <egorkyznetsov06@gmail.com>
2024-07-14 12:05:04 +0300

    База данных

--- run.py
+++ run.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
+
+# Создание подключения к базе данных
+conn = sqlite3.connect('posts.db')
+
+# Создание курсора
+cursor = conn.cursor()
+
+# Создание таблицы, если она еще не существует
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        content TEXT
+    )
+''')
+
+# Закрытие соединения
+conn.close()
 
 
 @app.route('/')
@@ -23,9 +42,34 @@ def nature_reserves():
 def recycling_points():
     return render_template("recycling_points.html")
 
-@app.route('/registration')
-def registration():
-    return render_template("registration.html")
+
+@app.route('/posts')
+def posts():
+    # Получение данных из базы данных
+    conn = sqlite3.connect('posts.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM posts")
+    posts = cursor.fetchall()
+    conn.close()
+    return render_template('posts.html', posts=posts)
+
+
+@app.route('/creature', methods=['GET', 'POST'])
+def creature():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        # Сохранение данных в базу данных
+        conn = sqlite3.connect('posts.db')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO posts (title, content) VALUES (?, ?)", (title, content))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('home'))
+
+    return render_template('creature.html')
 
 
 if __name__ == '__main__':
